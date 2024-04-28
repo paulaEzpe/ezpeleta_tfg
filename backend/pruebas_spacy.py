fd0ecc5689f87e0359dc8820f71ed2513ac65b00
paulaEzpe <800692@unizar.es>
2024-04-28 21:14:36 +0200

    algoritmo para comprobar las subsecuencias mas largas que coinciden entre parrafo y cita, implemenetado

--- backend/pruebas_spacy.py
+++ backend/pruebas_spacy.py
@@ -1,138 +1,98 @@
-# import spacy
-# import re
-# from fuzzywuzzy import fuzz
-# from autocorrect import Speller
+import spacy
+import re
 
-# ############Probando spaCy#############
+############Probando spaCy#############
 
-# nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm')
 
-# def obtain_english_words():
+# Texto con la palabra compuesta por guión
+text = "According to a recent census from 140 countries worldwide [1], breast cancer is the most frequently diagnosed cancer in women, with an estimated incidence of 1.7 million new cases per year. It represents 25\% \of all types of cancers diagnosed in women furthermore, 15\% \of cancer death cite3457345 formulaw47534t37hfhh [ ] , weq 234234"
 
-#     words = set(nlp.vocab.strings)
+cita = "According to a recent census from 140 countries worldwide, breast cancer is the most frequently diagnosed"
 
-#     # Texto con la palabra compuesta por guión
-#     text = "According to a recent census from 140 countries worldwide [1], breast cancer is the most frequently diagnosed cancer in women, with an estimated incidence of 1.7 million new cases per year. It represents 25\% \of all types of cancers diagnosed in women furthermore, 15\% \of cancer death cite3457345 formulaw47534t37hfhh [ ] , weq 234234"
+# dados dos strings, devuelve una lista con las palabras alfabéticas de cada uno de ellos
+def obtain_list_english_words(texto, cita):
 
-#     text_lowercase = re.sub(r'[A-Z]', lambda match: match.group().lower(), text)
+    # Poner en minúsculas el texto y la cita
+    text_lowercase = re.sub(r'[A-Z]', lambda match: match.group().lower(), text)
+    cite_lowercase = re.sub(r'[A-Z]', lambda match: match.group().lower(), cita)
 
-#     # Procesar el texto con el modelo de spaCy
-#     doc = nlp(text_lowercase)
+    # Procesar el texto y la cita con el modelo de spaCy
+    texto_procesado = nlp(text_lowercase)
+    cita_procesada = nlp(cite_lowercase)
 
-#     # Obtener todas las palabras alfabéticas detectadas por spaCy
-#     obtained_words = [token.text for token in doc if token.is_alpha]
+    # Obtener todas las palabras alfabéticas detectadas por spaCy
+    obtained_text_words = [token.text for token in texto_procesado if token.is_alpha]
+    obtained_cite_words = [token.text for token in cita_procesada if token.is_alpha]
 
-#     # Filtrar palabras reales que estén en el diccionario de palabras en inglés y convertirlas a minúsculas
-#     real_words = [word for word in obtained_words if word in words]
+    print("Lista de palabras del texto en inglés:")
+    print(obtained_text_words)
+    print("Lista de palabras de la cita en inglés:")
+    print(obtained_cite_words)
 
-#     print(real_words)
-#     return real_words
+    return obtained_text_words, obtained_cite_words
 
-
-# # def obtain_english_words2():
-
-# #     # Texto con la palabra compuesta por guión
-# #     text = "According to a recent census from 140 countries worldwide [1], breast cancer is the most frequently diagnosed cancer in women, with an estimated incidence of 1.7 million new cases per year. It represents 25\% \of all types of cancers diagnosed in women furthermore, 15\% \of cancer death cite3457345 formulaw47534t37hfhh [ ] , weq 234234"
-
-# #     text_lowercase = re.sub(r'[A-Z]', lambda match: match.group().lower(), text)
-
-# #     # Procesar el texto con el modelo de spaCy
-# #     doc = nlp(text_lowercase)
-
-# #     obtained_words = [token.text for token in doc if token.is_alpha]
-# #     found_words = [word for word in obtained_words if nlp(word)[0].is_oov == False]
-# #     print(found_words)
+# Llamada a la función
+#lista_texto_palabras, lista_cita_palabras = obtain_english_words(text, cita)
 
 
+#############################################################################################################
 
-# #obtain_english_words2()
+# Funcion que dados dos conjuntos, devuelve true si el primero de ellos contiene al menos el 80% de las palabras del segundo
+def contiene_suficientes_palabras(conjunto_parrafo, conjunto_cita):
 
-# ############Probando fuzzywuzzy#############
-
-# # Ejemplo de lista de palabras de la cita y cadena de texto del párrafo
-# from fuzzywuzzy import fuzz
-
-# palabras_parrafo = ['according', 'to', 'a', 'recent', 'census', 'from', 'countries', 'worldwide', 'breast', 'cancer', 'is', 'the', 'most', 'frequently', 'diagnosed', 'cancer', 'in', 'women', 'with', 'an', 'estimated', 'incidence', 'of', 'million', 'new', 'cases', 'per', 'year', 'it', 'represents', 'all', 'types', 'of', 'cancers', 'diagnosed', 'in', 'women', 'furthermore', 'cancer', 'death']
-# palabras_cita = ['cording', 'to', 'a', 'recent', 'census', 'from', 'countries', 'worldwide', 'breast', 'cancer', 'is', 'the', 'most', 'frequently', 'diagnosed', 'cancer', 'in', 'women', 'with', 'an', 'estimated', 'incidence', 'of', 'million', 'new', 'cases', 'per', 'year', 'it', 'represents', 'all', 'types', 'of', 'cancers', 'diagnosed', 'in', 'women', 'furthermore', 'cancer', 'dea']
-
-# def obtener_similitud(palabras_cita, palabras_parrafo):
-
-#     # Umbral de similitud para la primera y última palabra de la cita
-#     umbral_similitud_primera_ultima = 70
-
-#     # Verificar si hay alguna coincidencia entre las palabras de la cita y las del párrafo
-#     coincidencias_primera_ultima = (fuzz.ratio(palabras_cita[0], palabras_parrafo[0]) >= umbral_similitud_primera_ultima and
-#                                     fuzz.ratio(palabras_cita[-1], palabras_parrafo[-1]) >= umbral_similitud_primera_ultima)
-
-#     # Verificar coincidencia del 100% para las palabras entre la primera y la última
-#     coincidencias_intermedias = (palabras_cita[1:-1] == palabras_parrafo[1:-1])
-
-#     if coincidencias_primera_ultima and coincidencias_intermedias:
-#         print("Las listas coinciden")
-#     else:
-#         print("Las listas no coinciden")
-
-
-# ##########Probando el autocorrect#############
-
-# # # Lista de palabras incompletas
-# # lista_incompleta = ["naranj", "sill"]
-
-# # # Corregir las palabras incompletas en la lista
-# # corrector = Speller()
-# # lista_corregida = [corrector(palabra) for palabra in lista_incompleta]
-
-# # # Imprimir la lista corregida
-# # print("Lista corregida:")
-# # print(lista_corregida)
-
-# #obtain_english_words()
-# obtener_similitud(palabras_cita, palabras_parrafo)
-
-
-
-
-
-
-
-import os
-import requests
-import PyPDF2
-
-
-
-def descargar_pdf_arxiv(arxiv_id, directorio_destino):
-    # Construye la URL del PDF
-    url_pdf = f"https://arxiv.org/pdf/{arxiv_id}.pdf"
+    # Hacer un conteo de la cantidad de palabras de la cira que se encuentran en el parrafo
+    palabras_encontradas = sum(1 for palabra in conjunto_cita if palabra in conjunto_parrafo)
     
-    # Descarga el PDF
-    response = requests.get(url_pdf)
-    if response.status_code == 200:
-        
-        # Guarda el PDF en el directorio especificado
-        ruta_pdf = os.path.join(directorio_destino, f"{arxiv_id}.pdf")
-        with open(ruta_pdf, 'wb') as f:
-            f.write(response.content)
-        print(f"PDF descargado correctamente: {ruta_pdf}")
-        
-        # Extraer texto del PDF
-        texto = extraer_texto_pdf(ruta_pdf)
-        return texto, ruta_pdf  # Devuelve el texto extraído y la ruta del PDF descargado
+    # Calcular el porcentaje 
+    porcentaje_encontrado = (palabras_encontradas / len(conjunto_cita)) * 100
+    
+    if porcentaje_encontrado >= 80:
+        return True
     else:
-        print(f"No se pudo descargar el PDF. Código de estado: {response.status_code}")
-        return None, None  # Devuelve None si no se pudo descargar el PDF
+        return False
 
-def extraer_texto_pdf(ruta_pdf):
-    texto = ""
-    with open(ruta_pdf, 'rb') as f:
-        pdf_reader = PyPDF2.PdfReader(f)
-        for page in pdf_reader.pages:
-            texto += page.extract_text()
-    return texto
-# Ejemplo de uso
-texto_extraido, ruta_del_pdf = descargar_pdf_arxiv("1401.4766", "../datos/")
-print("Texto extraído del PDF:")
-print(texto_extraido)
+#############################################################################################################
+
+def algoritmo(lista_parrafo, lista_cita):
+    max_consecutivas = 0  # Inicializamos el máximo número de coincidencias consecutivas
+    max_subsecuencia = []  # Inicializamos la subsecuencia más larga que coincide
+    
+    if contiene_suficientes_palabras(set(lista_parrafo), set(lista_cita)):
+        # Iteramos sobre la lista de párrafo para considerar todas las subsecuencias posibles
+        for i in range(len(lista_parrafo)):
+            for j in range(i + 1, len(lista_parrafo) + 1):
+                subsecuencia = lista_parrafo[i:j]
+                
+                # Iteramos sobre todas las subsecuencias posibles de la lista de cita
+                for k in range(len(lista_cita)):
+                    for l in range(k + 1, len(lista_cita) + 1):
+                        subsecuencia_cita = lista_cita[k:l]
+                        
+                        # Si la subsecuencia del párrafo coincide con la subsecuencia de la cita, actualizamos max_consecutivas y max_subsecuencia
+                        if subsecuencia == subsecuencia_cita:
+                            if len(subsecuencia_cita) > max_consecutivas:
+                                max_consecutivas = len(subsecuencia_cita)
+                                max_subsecuencia = subsecuencia_cita
+    else: 
+        print("El párrafo no contiene suficientes palabras de la cita")
+        # devolverá 0 y la lista vacía
+    return max_consecutivas, max_subsecuencia
+    
+    
+# Llamada a la función
+lista_parrafo = ["hola", "que", "tal", "estas", "soy", "un", "párrafo", "con", "varias", "palabras", "que", "se", "repite", "que", "tal", "estas", "soy", "un", "párrafo", "con", "varias", "palabras", "que", "se", "repite", "que", "tal", "estas", "soy", "un", "párrafo", "con", "varias", "palabras", "que", "se", "repite"]
+lista_cita = ["que", "tal", "estas", "soy"]
+
+resultado = algoritmo(lista_parrafo, lista_cita)
+print("Máximo número de coincidencias consecutivas:", resultado)
+    
+
+
+
+
+
+
 
 
 
